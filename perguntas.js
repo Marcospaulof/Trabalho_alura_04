4d31a9ea7c1b4786742aaedcb10a431220000020
Marcospaulof <marcosdopovo2007@outlook.com>
2024-08-30 17:57:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,134 @@
+export const perguntas = [
+    {
+        enunciado: "Devemos permitir a democracia no império?",
+        alternativas: [
+            {
+                texto: "Sim podemos abrir para a democracia",
+                afirmacao:[ 
+                "O império se abriu para a democracia.",
+                
+            
+            ],
+            proxima: 1,
+            },
+            {
+                texto: "Não devemos manter a autoridade.",
+                afirmacao:[ 
+                "O império se manteu fechado.",
+                ],
+            proxima: 2,
+            }           
+
+        ]
+    },
+    {
+        enunciado: "Devemos deixar unipartidadrio ou pluripartidarios?",
+        alternativas: [
+            {
+                texto:"Unipartidario.",
+                afirmacao:[
+                "Foi criado o UND.",
+                "Mesmo com a criação do UND, a população ficou puta com a democracia limitada."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Pluripartidario.",
+                afirmacao:[
+                "Varios partidos foram criados.",
+                "Varois partidos foram criados muitos eram de oposição.",
+
+                ],
+                proxima: 3,
+            }
+        ]
+    },
+    {
+        enunciado: "Devemos ser uma autocracia ou criar alguns ministerios para nos ajudar?",
+        alternativas: [
+            {
+                texto:"Eu sou o estado!.",
+                afirmacao:[
+                "Todo mundo ficou puto.",
+                "Ninguem ficou feliz"
+
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Vamos  criar alguns ministérios para nós ajudar",
+                afirmacao:[
+                "Familiares firaram ministros",
+
+                ],
+                proxima: 3,
+            }
+        ]
+    },
+    {
+        enunciado: "Em 1910 descobrimos pescadores ilegais da Pranca, pescando nossos peixes!",
+        alternativas: [
+            {
+                texto:"Prender os pescadores",
+                afirmacao:[
+                "Os pesacadores foram presos.",
+                "Os pescadores consiguiram fugir."
+
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "Fazer reclmações a Pranca",
+                afirmacao:[
+                "Nada resolveu.",
+                "Eles não se importaram."
+
+                ],
+                proxima: 5,
+            }
+        ]
+    },
+    {
+        enunciado: "A Pranca está reclamando do incidente, eles dizem que a região é deles. O que deve ser feito?",
+        alternativas: [
+            {
+                texto:"CHUMBO NELES! (guerra contra os Planca)",
+                afirmacao:[
+                "Ganhamos!.",
+                "Perdemos!"
+
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Assinar acordos para uma resolução pacifica.",
+                afirmacao:[
+                "Os Pranca  nunca mais pescaram na região",
+                "Os Pranca concordaram em pagar taxas de pescas."
+
+                ],
+                proxima: 5,
+            }
+        ]
+    },
+    {
+        enunciado: "Esse é seu ultimo ato de você como impérador. Qual desses devem ser o proximo impérador?",
+        alternativas: [
+            {
+            texto:"Augustus Arabicus",
+                afirmacao:[
+                "Vida longa a Augustus Arabicus",
+
+                ],
+            },
+            {
+                texto: "Maximus Caffeinus",
+                afirmacao:[
+                "Vida longa a Maximus Caffeinus",
+
+                ],
+            }
+            
+        ]
+    },
+];
